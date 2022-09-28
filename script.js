32cb09c882390bc3dfb5408c747e0b171b9cbbf2
kmithunraj <kmithunraj@mithunraj.com>
2022-09-28 09:41:58 +0530

    web updatess

---
+++ script.js
@@ -0,0 +1,26 @@
+function showbutton1() {
+    var x = document.getElementById("showbanner1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showbutton2() {
+    var x = document.getElementById("showbanner2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showbutton3() {
+    var x = document.getElementById("showbanner3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
